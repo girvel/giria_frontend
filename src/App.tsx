56349b1625372ef06f6c66f492b996b630c84305
girvel <widauka@ya.ru>
2025-03-01 15:12:04 +0500

    Fetching the global map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
+import {useEffect, useState} from 'react';
 
-function App() {
+export default function App() {
+    const [globalMap, setGlobalMap] = useState(null);
+
+    useEffect(() => {
+        async function fetchGlobalMap() {
+          const response = await fetch("localhost:8080/global_map");
+          const json = await response.json();
+          console.log(json);
+          setGlobalMap(json);
+        }
+
+        fetchGlobalMap();
+    }, []);
+
     return (
-      <span>Hello, world!</span>
+      <span>{globalMap}</span>
     )
 }
-
-export default App
